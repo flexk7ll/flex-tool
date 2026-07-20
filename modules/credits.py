65d434fd0addb5f78beb40c85cd6eb09191b0056
flexk7ll <flexk7l.flex@gmail.com>
2026-07-20 22:15:24 +0300

    Add files via upload

---
+++ modules/credits.py
@@ -0,0 +1,18 @@
+LICENSE = """ by FLEX TEAM
+ Discord Server: https://discord.gg/rees7q7qX4  """
+
+from colored import fg
+
+r = fg(241) # Setup color variables
+r2 = fg(255)
+b = fg(31)
+w = fg(15)
+
+def show_credits():
+    
+    print(f" {r2}[{b}+{r2}]  YT: https://www.youtube.com/@8ijr")
+    print(f" {r2}[{b}+{r2}] Discord Server: https://discord.gg/rees7q7qX4")
+    print(f" {r2}[{b}+{r2}] GitHub: https://github.com/flexk7ll")
+
+def show_help():
+    pass
